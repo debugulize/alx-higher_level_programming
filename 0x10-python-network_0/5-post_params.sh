c5b2a0c5744e949b513f5a513f675de8053fc4d3
debugulize <wanjikumary631@gmail.com>
2022-09-22 04:20:55 -0700

    header

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s "$1" -X POST -d "test@gmail.com" -d "subject=I will always be here for PLD"
+# TAKES IN URL, POST AND SEND DATA
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
